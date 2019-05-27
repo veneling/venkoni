34d37de6973651e5e2b3942dc57b42772a2607e8
veneling <v.grozev@outlook.com>
2019-05-27 13:34:33 +0200

    reworked routing paths + module imports

--- src/client/app/app-routing.module.ts
+++ src/client/app/app-routing.module.ts
@@ -31,7 +31,6 @@ const routes: Routes = [
     MaterialModule,
     FormsModule,
     ReactiveFormsModule,
-    MaterialModule
   ],
   exports: [
     RouterModule
--- src/client/app/cvbuilder/main.module.ts
+++ src/client/app/cvbuilder/main.module.ts
@@ -6,12 +6,14 @@ import { AuthService } from '../auth/auth.service';
 import { AuthcactGuard } from '../auth/authcact.guard';
 import { LoginComponent } from '../auth/login/login.component';
 import { RegisterComponent } from '../auth/register/register.component';
+import { MaterialModule } from '../loaders/material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: '**', redirectTo: '' }
+  { path: '**', redirectTo: 'cvproject' }
 ]
 
 @NgModule({
@@ -23,6 +25,9 @@ const routes: Routes = [
   imports: [
     RouterModule.forChild(routes),
     CommonModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   exports: [
     CommonModule,
