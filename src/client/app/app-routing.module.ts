981b25d4a056b1e89effc5cfdf3faa1fe3fe0b4c
veneling <v.grozev@outlook.com>
2019-05-29 21:34:26 +0200

    styling and renaming fixes on the main routes

--- src/client/app/app-routing.module.ts
+++ src/client/app/app-routing.module.ts
@@ -5,19 +5,20 @@ import { MainModule } from './cvbuilder/main.module';
 import { MaterialModule } from './loaders/material.module';
 
 import { AboutComponent } from './navigation/about/about.component';
-import { CvComponent } from './navigation/cv/cv.component';
 import { ProjectsComponent } from './navigation/projects/projects.component';
 import { SidebarComponent } from './navigation/sidebar.component';
+import { ResumeComponent } from './navigation/resume/resume.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AuthcactGuard } from './cvbuilder/auth/authcact.guard';
 import { AuthService } from './cvbuilder/auth/auth.service';
 import { AuthcloadGuard } from './cvbuilder/auth/authcload.guard';
 
+
 const routes: Routes = [
   { path: '', component: SidebarComponent,
     children: [
       { path: 'about', component: AboutComponent },
-      { path: 'cv', component: CvComponent },
+      { path: 'resume', component: ResumeComponent },
       { path: 'projects' , component: ProjectsComponent }
     ]},
   { path: 'cvbuilder', loadChildren: () => MainModule },
@@ -37,7 +38,7 @@ const routes: Routes = [
   ],
   declarations: [ 
     SidebarComponent, 
-    AboutComponent, CvComponent, ProjectsComponent,
+    AboutComponent, ProjectsComponent, ResumeComponent
   ],
   providers:
   [
