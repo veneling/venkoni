ca731c00029d398d86f571a9499cb7951788cb97
veneling <v.grozev@outlook.com>
2019-05-30 20:00:36 +0200

    remove unnecessary user functions

--- src/server/users/users.controller.js
+++ src/server/users/users.controller.js
@@ -15,11 +15,8 @@ const jwtSecret = expressJwt({secret: JWT_SECRET})
 // routes
 router.post('/login', login)
 router.post('/register', register)
-router.get('/', jwtSecret, getAll)
-router.get('/current', jwtSecret, getCurrent)
-router.get('/:id', jwtSecret, getById)
-router.put('/:id', jwtSecret, update)
-router.delete('/:id', jwtSecret, _delete)
+router.get('/profile', jwtSecret, profile)
+
 
 module.exports = router;
 
@@ -112,32 +109,7 @@ async function login(req, res, next) {
         }
 }
 
-function getAll(req, res, next) {
-    userService.getAll()
-        .then(users => res.json(users))
-        .catch(err => next(err));
-}
-
-function getCurrent(req, res, next) {
-    userService.getById(req.user.sub)
-        .then(user => user ? res.json(user) : res.sendStatus(404))
-        .catch(err => next(err));
-}
-
-function getById(req, res, next) {
-    userService.getById(req.params.id)
-        .then(user => user ? res.json(user) : res.sendStatus(404))
-        .catch(err => next(err));
-}
-
-function update(req, res, next) {
-    userService.update(req.params.id, req.body)
-        .then(() => res.json({}))
-        .catch(err => next(err));
-}
-
-function _delete(req, res, next) {
-    userService.delete(req.params.id)
-        .then(() => res.json({}))
-        .catch(err => next(err));
+async function profile(req, res, next) {
+    const email = req.body.email
+    const user = await User.findOne({email: email})
 }
